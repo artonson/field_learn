3f5894aa267e6d81e413c5c932b584c59601b4e6
mtaktash <mariataktasheva@gmail.com>
2020-04-18 15:54:13 +0300

    Separate VectorImage from PolyVectorFieldGenerator

--- fieldlearn/data_generation/pvf_generator.py
+++ fieldlearn/data_generation/pvf_generator.py
@@ -1,70 +1,50 @@
 import torch
 import numpy as np
 from scipy.ndimage import gaussian_filter
-from svgpathtools import svg2paths
-from svgpathtools.path import translate, scale
-from fieldlearn.data_generation.utils import find_global_bbox, find_scale_factor, transform_slopes, inverse_transfom_slopes
+from fieldlearn.data_generation.utils import transform_slopes
+from vectran.data.graphics.graphics import VectorImage
+from vectran.renderers.cairo import render as cairo_render
 from vectran.optimization.optimizer.primitive_aligner import prepare_pixel_coordinates
 from vectran.optimization.primitives.quadratic_bezier_tensor import QuadraticBezierTensor
 
 
 def parse_bezier_paths(paths):
     control_points = []
+    widths = []
     for path in paths:
         for curve in path:
-            control_points.append([((p.real, p.imag),) for p in curve])
-    control_points = np.array(control_points).transpose(1, 2, 3, 0)
-    return control_points
+            control_points.append([((p.real, p.imag),) for p in curve.bpoints()])
+            widths.append(float(path.width))
+
+    # change axes:
+    #   num_primitives x num_control_points (3) x 1 x num_dimensions
+    #   ->
+    #   num_control_points x 1 x num_dimensions x num_primitives
+    control_points = np.array(control_points, dtype=np.float32).transpose(1, 2, 3, 0)
+    #  num_primitives x 1 -> 1 x 1 x num_primitives
+    widths = np.array(widths, dtype=np.float32).reshape(1, 1, -1)
+    return control_points, widths
 
 
-def parse_bezier_path_widths(paths_attr):
-    widths = []
-    for attr in paths_attr:
-        width_float = float(attr['stroke-width'].replace('px', ''))
-        widths.append(width_float)
-
-    widths = np.array(widths)
-    widths = widths.reshape(1, 1, -1)
-    return widths
-
-
-class PVFGenerator:
+class PolyVectorFieldGenerator:
+    # TODO: rewrite in terms of VectorImage
     def __init__(self,
-                 patch_height=64,
-                 patch_width=64,
                  smoothing_type='gaussian_smoothing',
                  gaussian_sigma=1,
-                 boundary_width=1,
-                 spatial_dims_n=2
                  ):
         """
         Generates PNG + a vector field array for an SVG image
 
         :param smoothing_type: ['no_smoothing', 'gaussian_smoothing']
         :param gaussian_sigma:
+        :param spatial_dims_n: number of dims in field
         """
-        self.patch_height = patch_height
-        self.patch_width = patch_width
-        self.boundary_width = boundary_width
         self.smoothing_type = smoothing_type
         self.gaussian_sigma = gaussian_sigma
-        self.spatial_dims_n = spatial_dims_n
+        self.spatial_dims_n = 2
         self.cardano_tol = 1e-2
-        self.cardano_division_eps = 1e-3
         self.similar_direction_tol = 1e-2
-
-    def normalize_paths(self, paths):
-        global_bbox = find_global_bbox(paths)
-        scale_factor = find_scale_factor(global_bbox, self.patch_height, self.patch_width, self.boundary_width)
-
-        normalized_paths = []
-        for path in paths:
-            normalized_paths.append(
-                scale(translate(
-                    path, np.complex(-global_bbox.xmin + self.boundary_width, -global_bbox.ymin + self.boundary_width)),
-                    scale_factor)
-            )
-        return normalized_paths
+        self.division_eps = 1e-3
 
     def gaussian_smoothing(self, field, mask):
         smoothed_field = field
@@ -74,14 +54,11 @@ class PVFGenerator:
         smoothed_field[2:][mask] = 0
         return smoothed_field
 
-    def generate_vector_field(self, svg_path, device=torch.device('cuda')):
-        paths, paths_attr = svg2paths(svg_path)
-        paths = self.normalize_paths(paths)
+    def generate_vector_field(self, img: VectorImage, renderer=cairo_render, device=torch.device('cuda')):
+        raster = img.render(renderer)
 
-        points = parse_bezier_paths(paths)
+        points, widths = parse_bezier_paths(img.paths)
         num_primitives = points.shape[-1]
-
-        widths = parse_bezier_path_widths(paths_attr)
 
         beziers = QuadraticBezierTensor(
             points[0],
@@ -90,17 +67,17 @@ class PVFGenerator:
             widths,
             dtype=torch.float32, device=device)
 
-        renders = beziers.render_with_cairo_each(self.patch_width, self.patch_height)
-        raster = (255 - torch.clamp(renders.sum(dim=1)[0] * 255, 0, 255)).type(torch.uint8).numpy()
+        renders = beziers.render_with_cairo_each(raster.shape[0], raster.shape[1])
+        # raster = (255 - torch.clamp(renders.sum(dim=1)[0] * 255, 0, 255)).type(torch.uint8).numpy()
 
         pixel_coords = prepare_pixel_coordinates(
-            torch.empty([1, self.patch_height, self.patch_width], dtype=torch.float32)).to(device)
+            torch.empty([1, raster.shape[0], raster.shape[1]], dtype=torch.float32)).to(device)
         canonical_x, canonical_y = beziers.calculate_canonical_coordinates(
-            pixel_coords, tol=self.cardano_tol, division_epsilon=self.cardano_division_eps)
+            pixel_coords, tol=self.cardano_tol, division_epsilon=self.division_eps)
 
         vector_field = beziers.get_vector_field_at(canonical_y)
         vector_field = vector_field[0].transpose(1, 0)
-        vector_field = vector_field.reshape(num_primitives, self.spatial_dims_n, self.patch_height, self.patch_width)
+        vector_field = vector_field.reshape(num_primitives, self.spatial_dims_n, raster.shape[0], raster.shape[1])
 
         mask = (renders.to(device) > 0)
         x = vector_field[:, 0]
@@ -113,7 +90,7 @@ class PVFGenerator:
         field.real[~np.isnan(field_x)] = field_x[~np.isnan(field_x)]
         field.imag[~np.isnan(field_y)] = field_y[~np.isnan(field_y)]
 
-        result = np.full((self.spatial_dims_n, self.patch_height, self.patch_width), 0+0j, dtype=np.complex64)
+        result = np.full((self.spatial_dims_n, raster.shape[0], raster.shape[1]), 0+0j, dtype=np.complex64)
 
         for primitive_idx in range(num_primitives):
             tangent_field = field[0][primitive_idx]
@@ -133,7 +110,7 @@ class PVFGenerator:
         mask = np.isclose(result, 0+0j)
         c_0, c_2 = transform_slopes(u, v)
 
-        field = np.zeros(shape=(4, self.patch_height, self.patch_width), dtype=np.float32)
+        field = np.zeros(shape=(4, raster.shape[0], raster.shape[1]), dtype=np.float32)
         field[0], field[1] = c_0.real, c_0.imag
         field[2], field[3] = c_2.real, c_2.imag
 
@@ -143,4 +120,4 @@ class PVFGenerator:
         field[:2] /= (np.linalg.norm(field[:2], axis=0) + 1e-5)
         field[2:] /= (np.linalg.norm(field[2:], axis=0) + 1e-5)
 
-        return raster, field
+        return field
--- fieldlearn/data_generation/utils.py
+++ fieldlearn/data_generation/utils.py
@@ -1,35 +1,3 @@
-from collections import namedtuple
-
-
-BoundingBox = namedtuple('BoundingBox', ['xmin', 'xmax', 'ymin', 'ymax'])
-
-
-def join_bboxes(first, second):
-    return BoundingBox(
-        min(first.xmin, second.xmin),
-        max(first.xmax, second.xmax),
-        min(first.ymin, second.ymin),
-        max(first.ymax, second.ymax)
-    )
-
-
-def find_global_bbox(paths):
-    global_bbox = BoundingBox(*paths[0].bbox())
-    for path in paths:
-        global_bbox = join_bboxes(global_bbox, BoundingBox(*path.bbox()))
-    return global_bbox
-
-
-def find_scale_factor(global_bbox, height, width, boundary_size):
-    xdiff = global_bbox.xmax - global_bbox.xmin
-    ydiff = global_bbox.ymax - global_bbox.ymin
-
-    if xdiff > ydiff:
-        return width / (xdiff + boundary_size)
-    else:
-        return height / (ydiff + boundary_size)
-
-
 def transform_slopes(u, v):
     c_0 = (u ** 2) * (v ** 2)
     c_2 = -(u ** 2 + v ** 2)
